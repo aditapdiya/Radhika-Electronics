5f425a6d4124aa6cc667962c5bbb4ea47771a872
aditapdiya <aditapdiya@gmail.com>
2026-06-17 14:57:46 +0530

    Remove superuser creation logic from MainConfig class

--- main/apps.py
+++ main/apps.py
@@ -6,23 +6,3 @@ class MainConfig(AppConfig):
 
     def ready(self):
         import main.signals 
-
-
-
-from django.contrib.auth import get_user_model
-from main.models import UserProfile
-
-User = get_user_model()
-
-if not User.objects.filter(username='admin').exists():
-    user = User.objects.create_superuser(
-        username='admin',
-        email='admin@example.com',
-        password='Admin@123'
-    )
-
-    UserProfile.objects.create(
-        user=user,
-        mobile='9876543210',
-        address='Admin Address'
-    )
